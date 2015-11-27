6051b14f48c00180f8856a9964a170867f6ea3a3
danielee <10915819@qq.com>
2015-11-27 14:54:56 +0800

    init
    添加了两个例子。

---
+++ TTTAttributedLabelDemo/TTTAttributedLabelDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTTAttributedLabelDemo
+//
+//  Created by chanli on 15/11/27.
+//  Copyright © 2015年 aotumanlady Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TTTAttributedLabelDemo/TTTAttributedLabelDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TTTAttributedLabelDemo
+//
+//  Created by chanli on 15/11/27.
+//  Copyright © 2015年 aotumanlady Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
